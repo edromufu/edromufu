dc02cae7fa935093fd3a22b343ae0361149eb354
Paulo Henrique Barbosa Botelho <70411064+Paulo-Botelho@users.noreply.github.com>
2023-02-12 16:07:30 -0300

    Update index.js

--- edrom-docs/src/components/HomepageFeatures/index.js
+++ edrom-docs/src/components/HomepageFeatures/index.js
@@ -13,7 +13,7 @@ const FeatureList = [{
     },
     {
         title: 'EDROM',
-        Svg: require('@site/static/img/logo-edrom.svg').default,
+        Svg: require('@site/static/img/logo-edrom.png').default,
         description: ( <
             >
             Equipe de Desenvolvimento em Robótica Móvel da Universidade Federal de Uberlândia. <
