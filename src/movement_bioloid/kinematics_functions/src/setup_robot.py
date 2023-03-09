104c658ae0d7b823c85e3f1553dcee30c1a7e32f
Luis Costa <lipemenezescosta@gmail.com>
2023-03-09 19:57:40 -0300

    Importação de .json no código de setup de robô, além de descrição da nova robô em .json.

---
+++ src/movement_bioloid/kinematics_functions/src/setup_robot.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+#coding=utf-8
+
+import json, os
+
+from joint import Joint
+
+class Robot:
+
+    def __init__(self, robot):
+        self.setupRobot(robot)
+        self.findMother()
+
+        self.robotJoints[0].absolutePostion = self.robotJoints[0].get_mother2SelfVec()
+        self.robotJoints[0].absolutePosture = np.identity(3)
+        
+    def setupRobot(self, robot):
+        self.loadJson(robot+'.json')
+        
+        self.robotJoints = []
+
+        for joint_data in self.json_data['leg_joints']:
+            self.robotJoints.append(Joint(*joint_data.values()))
+        
+    def loadJson(self, fileName):
+        os.chdir('/home/kirlin/edromufu/src/movement_bioloid/humanoid_properties/jsons/')
+        with open(fileName) as f:
+            self.json_data = json.loads(f.read())
+    
+    def findMother(self,j=0):
+        if j != -1:
+            if j == 0:
+                self.robotJoints[j].set_mom(-1)
+            
+            if self.robotJoints[j].get_child() != -1:
+                self.robotJoints[self.robotJoints[j].get_child()].set_mom(j)
+                self.findMother(self.robotJoints[j].get_child())
+            
+            if self.robotJoints[j].get_sister() != -1:
+                self.robotJoints[self.robotJoints[j].get_sister()].set_mom(self.robotJoints[j].get_mom())
+                self.findMother(self.robotJoints[j].get_sister())
+    
+if __name__ == '__main__':
+
+    nova = Robot('nova')
