5a8c0f59434cc025c5a488e4d01b87d367cfe084
Luis Costa <lipemenezescosta@gmail.com>
2023-03-11 01:38:02 -0300

    Adição da cinemática direta funcional, com teste no setup_robot.py.

--- src/movement_bioloid/humanoid_definition/src/setup_robot.py
+++ src/movement_bioloid/humanoid_definition/src/setup_robot.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 #coding=utf-8
 
-import json, os
+import json, os, sys
 import numpy as np
 
 from joint import Joint
+
+os.chdir(os.path.dirname(__file__))
+os.chdir("../../kinematic_functions/src")
+sys.path.append(os.getcwd())
+import direct_kinematics
 
 class Robot:
 
@@ -44,6 +49,15 @@ class Robot:
                 self.robotJoints[self.robotJoints[j].get_sister()].set_mom(self.robotJoints[j].get_mom())
                 self.findMother(self.robotJoints[j].get_sister())
     
-if __name__ == '__main__':
+    def runKinematics(self):
+        direct_kinematics.ForwardKinematics(self.robotJoints)
 
+        self.robotJoints[4].jointRotation = np.pi/2
+        direct_kinematics.ForwardKinematics(self.robotJoints)
+
+        for joint in self.robotJoints:
+            print(f'{joint.get_name()}: \n{joint.absolutePostion}\n')
+
+if __name__ == '__main__':
     nova = Robot('nova')
+    nova.runKinematics()
