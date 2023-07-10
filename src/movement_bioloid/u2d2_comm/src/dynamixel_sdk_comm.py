8782f9ac2fe64c67eecbcff7c49a74243a8b8d85
Luis Costa <lipemenezescosta@gmail.com>
2023-07-10 17:11:50 -0300

    Alterações no append não funcional da coree  na comunicação com a u2d2.

--- src/movement_bioloid/u2d2_comm/src/dynamixel_sdk_comm.py
+++ src/movement_bioloid/u2d2_comm/src/dynamixel_sdk_comm.py
@@ -13,15 +13,12 @@ BAUDRATE = 1000000
 DEVICENAME = rospy.get_param('u2d2/port')
 
 ADDR_TORQUE_ENABLE = 24
-
 ADDR_LED_ENABLE = 25
 
 ADDR_GOAL_POSITION = 30
 GOAL_POSITION_LENGTH = 2
 
 ADDR_PRESENT_POSITION = 36
-
-ADDR_MOVING = 46
 
 class u2d2Control():
 
@@ -30,22 +27,22 @@ class u2d2Control():
         
         rospy.Subscriber('u2d2_comm/data2body', body_motors_data, self.data2body)
 
-        #rospy.Subscriber('u2d2_comm/data2head', head_motors_data, self.data2head)
+        rospy.Subscriber('u2d2_comm/data2head', head_motors_data, self.data2head)
 
         rospy.Service('u2d2_comm/enableTorque', enable_torque, self.enableTorque)
         self.enableTorqueRes = enable_torqueResponse()
 
-        rospy.Service('u2d2_comm/feedbackMotors', position_feedback, self.feedbackMotors)
-        self.feedbackRes = position_feedbackResponse()
+        rospy.Service('u2d2_comm/feedbackBody', body_feedback, self.feedbackBodyMotors)
+        self.bodyFeedbackRes = body_feedbackResponse()
+
+        rospy.Service('u2d2_comm/feedbackHead', head_feedback, self.feedbackHeadMotors)
+        self.headFeedbackRes = head_feedbackResponse()
 
         self.portHandler = PortHandler(DEVICENAME)
         self.packetHandler = PacketHandler(PROTOCOL_VERSION)
 
         self.bodyGroup = GroupSyncWrite(self.portHandler, self.packetHandler, ADDR_GOAL_POSITION, GOAL_POSITION_LENGTH)
         self.headGroup = GroupSyncWrite(self.portHandler, self.packetHandler, ADDR_GOAL_POSITION, GOAL_POSITION_LENGTH)
-        self.feedbackGroup = GroupSyncRead(self.portHandler, self.packetHandler, ADDR_GOAL_POSITION, GOAL_POSITION_LENGTH)
-        for motor_id in range(20):
-            _=self.feedbackGroup.addParam(motor_id)
 
         self.startComm()
 
@@ -68,52 +65,51 @@ class u2d2Control():
     
     def enableTorque(self, req):
         
-        if req.motor_ids[0] == -1:
-            motor_ids = range(20)
-        else:
-            motor_ids = req.motor_ids
-
-        success_motors = []
-        failure_motors = []
-
-        for motor_id in motor_ids:
-            self.packetHandler.write1ByteTxOnly(self.portHandler, motor_id, ADDR_TORQUE_ENABLE, req.data)
-            self.packetHandler.write1ByteTxOnly(self.portHandler, motor_id, ADDR_LED_ENABLE, req.data)
-            torque_status, _comm, _hard = self.packetHandler.read1ByteTxRx(self.portHandler, motor_id, ADDR_TORQUE_ENABLE)
-
-            if torque_status != req.data:
-                failure_motors.append(motor_id)
+        for _ in range(3):
+            if req.motor_ids[0] == -1:
+                motor_ids = range(20)
             else:
-                success_motors.append(motor_id)
+                motor_ids = req.motor_ids
+            
+            for motor_id in motor_ids:
+                self.packetHandler.write1ByteTxOnly(self.portHandler, motor_id, ADDR_TORQUE_ENABLE, req.data)
+                self.packetHandler.write1ByteTxOnly(self.portHandler, motor_id, ADDR_LED_ENABLE, req.data)
 
-        self.enableTorqueRes.message = f'Sucesso: {success_motors}   |   Falha: {failure_motors}'
-        if len(success_motors) == len(motor_ids):
             self.enableTorqueRes.success = True
-        else:
-            self.enableTorqueRes.success = False
         
         return self.enableTorqueRes
 
-    def feedbackMotors(self, req):
+    def feedbackBodyMotors(self, req):
         try:
+            self.bodyFeedbackRes.pos_vector = [0]*18
 
-            self.feedbackRes.pos_vector = [0]*20
-            self.feedbackGroup.txRxPacket()
-            for motor_id in range(20):
-                print(self.feedbackGroup.isAvailable(motor_id,ADDR_GOAL_POSITION, GOAL_POSITION_LENGTH)) #! Debug?
+            for motor_id in range(18):
+                motor_position, comm, hard = self.packetHandler.read2ByteTxRx(self.portHandler, motor_id, ADDR_PRESENT_POSITION)
 
-                if self.feedbackGroup.isAvailable(motor_id,ADDR_GOAL_POSITION, GOAL_POSITION_LENGTH):
-                    motor_pos= self.feedbackGroup.getData( motor_id,ADDR_GOAL_POSITION, GOAL_POSITION_LENGTH)
-
-                    print(motor_pos)    #! Debug?           
-
-                    self.feedbackRes.pos_vector[motor_id] = self.pos2rad(motor_pos)
+                if comm !=0 or hard != 0:
+                    self.bodyFeedbackRes.pos_vector[motor_id] = -1
                 else:
-                    self.feedbackRes.pos_vector[motor_id] = -1
-                
-            return self.feedbackRes
+                    self.bodyFeedbackRes.pos_vector[motor_id] = self.pos2rad(motor_position)
+
+            return self.bodyFeedbackRes
         except:
-            return self.feedbackMotors(req)
+            return self.feedbackBodyMotors(req)
+    
+    def feedbackHeadMotors(self, req):
+        try:
+            self.headFeedbackRes.pos_vector = [0]*2
+
+            for motor_id in range(2):
+                motor_position, comm, hard = self.packetHandler.read2ByteTxRx(self.portHandler, motor_id+18, ADDR_PRESENT_POSITION)
+
+                if comm !=0 or hard != 0:
+                    self.headFeedbackRes.pos_vector[motor_id] = -1
+                else:
+                    self.headFeedbackRes.pos_vector[motor_id] = self.pos2rad(motor_position)
+
+            return self.headFeedbackRes
+        except:
+            return self.feedbackHeadMotors(req)
 
     def data2body(self, msg):
         self.bodyGroup.clearParam()
@@ -137,7 +133,7 @@ class u2d2Control():
             value = self.rad2pos(motor_position)
             bytes_value = value.to_bytes(2, byteorder='little')
 
-            self.headGroup.addParam(motor_id, bytes_value)
+            self.headGroup.addParam(motor_id+18, bytes_value)
 
         self.headGroup.txPacket()
     
