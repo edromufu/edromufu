49d0aec3725062788fc3e53187584d9e95246fe4
Luis Costa <lipemenezescosta@gmail.com>
2023-09-07 18:55:39 -0300

    Page da natasha e atualização na core de QOL.

--- src/movement_bioloid/movement_core/src/core.py
+++ src/movement_bioloid/movement_core/src/core.py
@@ -25,37 +25,40 @@ PUB2VIS = rospy.get_param('/movement_core/pub2vis')
 
 class Core:
     def __init__(self): 
-
-        rospy.wait_for_service('u2d2_comm/feedbackBody')
-        rospy.wait_for_service('u2d2_comm/enableTorque')
-        
-        #Inicialização do objeto (modelo) da robô em código
-        robot_name = rospy.get_param('/movement_core/name')
-                
-        self.robotInstance = Robot(robot_name)
-        self.robotModel = self.robotInstance.robotJoints
-        self.motorId2JsonIndex = self.robotInstance.motorId2JsonIndex
         
         # Inicialização das variáveis do ROS
         rospy.init_node('movement_central')
+        
+        # Inicialização das variáveis do ROS para u2d2
+        if rospy.get_param('/movement_core/wait4u2d2'):
+            rospy.wait_for_service('u2d2_comm/feedbackBody')
+            rospy.wait_for_service('u2d2_comm/enableTorque')
+        
+            #Estruturas para comunicação com U2D2
+            self.motorsFeedback = rospy.ServiceProxy('u2d2_comm/feedbackBody', body_feedback)
+            self.motorsTorque = rospy.ServiceProxy('u2d2_comm/enableTorque', enable_torque)
+            self.pub2motors = rospy.Publisher('u2d2_comm/data2body', body_motors_data, queue_size=100)
+            self.pub2motorsMsg = body_motors_data()
+
+            #Inicialização do torque
+            self.motorsTorque(True, [-1])
+
+            self.queue = []
 
         #Services de requisição de movimento, todos possuem como callback movementManager
         rospy.Service('movement_central/request_gait', gait, self.movementManager)
         rospy.Service('movement_central/request_page', page, self.movementManager)
+
+        #Inicialização do objeto (modelo) da robô em código
+        robot_name = rospy.get_param('/movement_core/name')
+
+        self.robotInstance = Robot(robot_name)
+        self.robotModel = self.robotInstance.robotJoints
+        self.motorId2JsonIndex = self.robotInstance.motorId2JsonIndex
         
-        #Estruturas para comunicação com U2D2
-        self.motorsFeedback = rospy.ServiceProxy('u2d2_comm/feedbackBody', body_feedback)
-        self.motorsTorque = rospy.ServiceProxy('u2d2_comm/enableTorque', enable_torque)
-        self.pub2motors = rospy.Publisher('u2d2_comm/data2body', body_motors_data, queue_size=100)
-        self.pub2motorsMsg = body_motors_data()
-
-        #Inicialização do torque
-        self.motorsTorque(True, [-1])
-
         #Timer para fila de publicações
         rospy.Timer(rospy.Duration(QUEUE_TIME), self.sendFromQueue)
-        self.queue = []
-
+        
         # Definições para visualizador
         if PUB2VIS:
             self.queuevis = []
@@ -119,13 +122,14 @@ class Core:
             checked_poses = np.array([self.motorsCurrentPosition])
             gait_poses = Gait(self.robotModel, req.step_height, req.steps_number)
             
-            for index, pose in enumerate(gait_poses):
-                pose = self.invertMotorsPosition(pose)
-                pose = self.sortJsonIndex2MotorInput(pose)
-                checked_poses = np.append(checked_poses, [pose], axis=0)  
+            if rospy.get_param('/movement_core/wait4u2d2'):
+                for index, pose in enumerate(gait_poses):
+                    pose = self.invertMotorsPosition(pose)
+                    pose = self.sortJsonIndex2MotorInput(pose)
+                    checked_poses = np.append(checked_poses, [pose], axis=0)  
 
-            for pose in checked_poses: 
-                self.queue.append(pose)
+                for pose in checked_poses: 
+                    self.queue.append(pose)
 
             if PUB2VIS:
                 for pose in gait_poses:
@@ -138,8 +142,9 @@ class Core:
         elif 'page' in str(req.__class__):
             page_poses = Page(req.page_name, QUEUE_TIME)
             
-            for pose in page_poses: 
-                self.queue.append(pose)
+            if rospy.get_param('/movement_core/wait4u2d2'):
+                for pose in page_poses: 
+                    self.queue.append(pose)
             
             if PUB2VIS:
                 for pose in page_poses:
@@ -154,16 +159,17 @@ class Core:
         return response
     
     def sendFromQueue(self, event):
+        
+        if rospy.get_param('/movement_core/wait4u2d2'):
+            if self.queue:
+                self.pub2motorsMsg.pos_vector = self.queue.pop(0)
+                self.pub2motors.publish(self.pub2motorsMsg)
 
-        if self.queue:
-            self.pub2motorsMsg.pos_vector = self.queue.pop(0)
-            self.pub2motors.publish(self.pub2motorsMsg)
-
-        if self.queuevis and PUB2VIS:
-            self.pub2vismsg.position = self.queuevis.pop(0)
-            self.pub2vismsg.header.stamp = rospy.Time.now()
-            self.pub2vis.publish(self.pub2vismsg)
-
+        if PUB2VIS:
+            if self.queuevis:
+                self.pub2vismsg.position = self.queuevis.pop(0)
+                self.pub2vismsg.header.stamp = rospy.Time.now()
+                self.pub2vis.publish(self.pub2vismsg)
 
 if __name__ == '__main__':
     np.set_printoptions(precision=3, suppress=True, linewidth=np.inf, threshold=sys.maxsize)
