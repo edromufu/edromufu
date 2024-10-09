60a57226923322c26e2bcc38a476d40c169025d0
Martins-Lucaas <lucaspmartins14@gmail.com>
2024-10-09 16:31:48 -0300

    atualizações para comunicação com o gamecontrole em statemachine

--- src/behaviour/transitions_and_states/transitions_and_states/goalkeeper_behaviour.py
+++ src/behaviour/transitions_and_states/transitions_and_states/goalkeeper_behaviour.py
@@ -2,52 +2,51 @@
 #coding=utf-8
 
 import numpy as np
-
-#import rospy, os, sys,time (1)
-import rclpy, os, sys,time #(1)
+import rclpy, os, sys, time
 from vision_msgs.msg import Webotsmsg
 from movement_utils.srv import *
 from movement_utils.msg import *
+from modularized_bhv_msgs.msg import GameControllerMsg  # Mensagem do GameController
 
-edrom_dir = '/home/'+os.getlogin()+'/edromufu/src/'
+edrom_dir = '/home/' + os.getlogin() + '/edromufu/src/'
 
-sys.path.append(edrom_dir+'behaviour/transitions_and_states/src')
+sys.path.append(edrom_dir + 'behaviour/transitions_and_states/src')
 from behaviour_parameters import BehaviourParameters
-
 
 class goalkeeper_brain:
     def __init__(self):
-
-        #rospy.init_node('goalkeeper_brain') (2)
-        rclpy.init(args=sys.argv)           #(2)
+        rclpy.init(args=sys.argv)
         self.node = rclpy.create_node('goalkeeper_brain')
 
         self.parameters = BehaviourParameters()
 
-        #rospy.wait_for_service('u2d2_comm/feedbackHead')
-        #self.motorsFeedback = rospy.ServiceProxy('u2d2_comm/feedbackHead', head_feedback) (3)
-        #self.pageCall = rospy.ServiceProxy('movement_central/request_page', page) (4)
+        self.motorsFeedback = self.node.create_client(HeadFeedback, 'u2d2_comm/feedbackHead')
+        self.pageCall = self.node.create_client(Page, 'movement_central/request_page')
 
-        self.motorsFeedback = self.node.create_client(HeadFeedback, 'u2d2_comm/feedbackHead') #(3)
-        self.pageCall = self.node.create_client(Page, 'movement_central/request_page') #(4)
+        while not self.motorsFeedback.wait_for_service(timeout_sec=1.0):
+            self.node.get_logger().info('Service not available, waiting again...')
+        while not self.pageCall.wait_for_service(timeout_sec=1.0):
+            self.node.get_logger().info('Service not available, waiting again...')
 
-        while not self.motorsFeedback.wait_for_service(timeout_sec=1.0): #(3)
-            self.node.get_logger().info('service not available, waiting again...')
-        while not self.pageCall.wait_for_service(timeout_sec=1.0): #(4)
-            self.node.get_logger().info('service not available, waiting again...')
+        # Subscribes para as mensagens de visão e dos motores
+        self.node.create_subscription(Webotsmsg, self.parameters.vision2BhvTopic, self.updateBallParameters, 10)
+        self.node.create_subscription(HeadMotorsData, self.parameters.headPositionsTopic, self.updateHorRotation, 10)
 
-        #rospy.Subscriber(self.parameters.vision2BhvTopic, Webotsmsg, self.updateBallParameters) (5)
-        #rospy.Subscriber(self.parameters.headPositionsTopic, head_motors_data, self.updateHorRotation) (6)
+        # Subscriber para o GameController
+        self.node.create_subscription(GameControllerMsg, 'Game_Controller', self.game_controller_callback, 10)
 
-        self.node.create_subscription(Webotsmsg, self.parameters.vision2BhvTopic, self.updateBallParameters, rclpy.qos.QoSProfile()) #(5)
-        self.node.create_subscription(HeadMotorsData, self.parameters.headPositionsTopic, self.updateHorRotation, rclpy.qos.QoSProfile()) #(6)
-
+        # Variáveis de estado
         self.found = False
         self.x = 0
         self.y = 0
- 
         self.timesFoundFalse = 0
+        self.game_controller_state = None  #Variável para controlar o estado do gamecontroller
     
+    def game_controller_callback(self, msg):
+ 
+        self.game_controller_state = msg.game_state
+        self.node.get_logger().info(f'Estado do GameController: {self.game_controller_state}')
+
     def updateBallParameters(self, msg):
         ballInfos = msg.ball
 
@@ -56,46 +55,52 @@ class goalkeeper_brain:
             if self.timesFoundFalse == 3:
                 self.found = False
                 self.timesFoundFalse = 0
-
         else:
             self.found = True
             self.x = ballInfos.x
             self.y = ballInfos.y
-            self.ballClose = True if self.y>self.parameters.yCenterBottomLimit else False
+            self.ballClose = True if self.y > self.parameters.yCenterBottomLimit else False
             self.timesFoundFalse = 0
             self.hasReceivedVision = True
     
     def updateHorRotation(self, msg):
-        self.HorRotation,VerRotation = msg.pos_vector
+        self.HorRotation, VerRotation = msg.pos_vector
     
-    def run():
-        #while not rospy.is_shutdown(): (7)
-        while not rclpy.ok(): #(7)
+    def run(self):
+        while rclpy.ok():
 
-            if self.found:
-                self.pageCall('natasha_squat')
+            if self.game_controller_state == 3:  # Playing
+                if self.found:
+                    self.pageCall('natasha_squat')
 
-            elif self.found and self.ballClose:
-                # >0 Direita e <0 esquerda
+                elif self.found and self.ballClose:
+                    # >0 Direita e <0 esquerda
+                    if self.HorRotation < self.parameters.lookingLeftRad / 2:
+                        self.pageCall('natasha_left_defense')
+                        self.fall()
+                    elif self.HorRotation > self.parameters.lookingRightRad / 2:
+                        self.pageCall('natasha_right_defense')
+                        self.fall()
+                    else:
+                        pass
+
+
+                        #CONTROLADORES PARA INTERAÇÃO DO GAMECONTROLLER (ADICIONAR PAGES AQUI)
+            elif self.game_controller_state == 1:  # Ready
+                self.node.get_logger().info('GameController: Ready - Preparando para começar')
+
+            elif self.game_controller_state == 2:  # Set
+                self.node.get_logger().info('GameController: Set - Posicionando-se')
                 
-                if self.HorRotation < self.parameters.lookingLeftRad/2:              
-                    self.pageCall('natasha_left_defense')
-                    self.fall()
-                elif self.HorRotation > self.parameters.lookingRighttRad/2:
-                    self.pageCall('natasha_right_defense')
-                    self.fall()
-                else:
-                    pass
-    
-    def fall():
-        #while not rospy.is_shutdown(): (8)
-        while not rclpy.ok(): #(8)
+            elif self.game_controller_state == 4:  # Finished
+                self.node.get_logger().info('GameController: Finished - Parando ações')
+
+    def fall(self):
+        while rclpy.ok():
             self.pageCall('fallen_natasha')
                 
+
 if __name__ == '__main__':
-    #goalkeeper_brain = goalkeeper_brain() (9)
-    goalkeeper = goalkeeper_brain() #(9)
-    #goalkeeper_brain.run() (10)
-    goalkeeper.run() #(10)
-    #rospy.spin() (11)
-    rclpy.spin(goalkeeper.node) #(11)
+    goalkeeper = goalkeeper_brain()
+    goalkeeper.run()
+    rclpy.spin(goalkeeper.node)
