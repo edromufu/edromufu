8baf959529df21bb384b442d96b631e08de7473e
analaura <analauraguastali@hotmail.com>
2024-10-29 20:51:39 -0300

    modificação maquinas de estado

--- src/behaviour/transitions_and_states/transitions_and_states/state_machine.py
+++ src/behaviour/transitions_and_states/transitions_and_states/state_machine.py
@@ -40,7 +40,7 @@ from behaviour_parameters import BehaviourParameters
 
 class StateMachine():
 
-    def __init__(self):
+    def _init_(self):
         """
         Construtor:
         - Define a lista de possiveis estados
@@ -49,24 +49,34 @@ class StateMachine():
         - Inicializa a maquina de estados, utilizando as caracteristicas ja criadas
         """
 
-        states = ['walking', 'stand_still', 'getting_up', 'impossible']
+        states = ['walking', 'stand_still', 'getting_up', 'impossible','initial','ready','set','play','finished']
 
         go_to_walking_transitions = [
             {'trigger': 'go_to_walking', 'source': 'walking', 'dest': 'walking',
              'conditions': 'walking_condition', 'unless': 'getting_up_condition'},
             {'trigger': 'go_to_walking', 'source': 'stand_still', 'dest': 'walking',
+             'conditions': 'walking_condition', 'unless': 'getting_up_condition'},
+            {'trigger': 'go_to_walking', 'source': 'play', 'dest': 'walking',
              'conditions': 'walking_condition', 'unless': 'getting_up_condition'}
         ]
+            
+        
 
         go_to_stand_still_transitions = [
-            {'trigger': 'go_to_stand_still', 'source': 'stand_still', 'dest': 'stand_still',
+            {'trigger': 'go_to_stand_still', 'source': 'set', 'dest': 'stand_still',
              'unless': 'getting_up_condition'},
             {'trigger': 'go_to_stand_still', 'source': 'getting_up', 'dest': 'stand_still',
+             'unless': 'getting_up_condition'},
+            {'trigger': 'go_to_stand_still', 'source': 'finished', 'dest': 'stand_still',
+             'unless': 'getting_up_condition'},
+            {'trigger': 'go_to_stand_still', 'source': 'stand_still', 'dest': 'stand_still',
              'unless': 'getting_up_condition'}
         ]
 
         go_to_getting_up_transitions = [
             {'trigger': 'go_to_getting_up', 'source': '*', 'dest': 'getting_up',
+             'conditions': 'getting_up_condition'},
+            {'trigger': 'go_to_getting_up', 'source': 'play', 'dest': 'getting_up',
              'conditions': 'getting_up_condition'}
         ]
 
@@ -76,15 +86,39 @@ class StateMachine():
             {'trigger': 'go_to_getting_up', 'source': '*', 'dest': 'impossible',
              'conditions': 'impossible_condition'},
             {'trigger': 'got_to_stand_still', 'source': '*', 'dest': 'impossible',
+             'conditions': 'impossible_condition'},
+            {'trigger': 'ready_source','source': 'set','dest':'impossible',
+             'condition':'impossible_condition'},
+            {'trigger': 'go_to_walking','source': 'ready','dest':'impossible',
+             'condition':'impossible_condition'},
+            {'trigger': 'go_to_walking', 'source': 'set', 'dest': 'impossible',
+             'conditions': 'impossible_condition'},
+            {'trigger': 'go_to_walking', 'source': 'finished', 'dest': 'impossible',
+             'conditions': 'impossible_condition'},
+            {'trigger': 'getting_up', 'source': 'initial', 'dest': 'impossible',
+             'conditions': 'impossible_condition'},
+            {'trigger': 'getting_up', 'source': 'ready', 'dest': 'impossible',
+             'conditions': 'impossible_condition'},
+            {'trigger': 'getting_up', 'source': 'set', 'dest': 'impossible',
              'conditions': 'impossible_condition'}
+            
         ]
-        #go_to penalty_kick
-
-        #go_to_start_game
+        game_controle_transitions = [
+            {'trigger': 'iniciar_jogo','source': '*','dest':'initial',
+             'condition':'initial_condition'},
+            {'trigger': 'ready_game','source': 'initial','dest':'ready',
+             'condition':'ready_condition'},
+            {'trigger': 'set_game','source': 'ready','dest':'set',
+             'condition':'set_condition'},
+            {'trigger': 'play_game','source': 'set','dest':'play',
+             'condition':'play_condition'},
+            {'trigger': 'finished_game','source': 'play','dest':'finished',
+             'condition':'finished_condition'}
+        ]
 
         all_transitions = (go_to_walking_transitions
                            + go_to_getting_up_transitions + go_to_stand_still_transitions
-                           + go_to_impossible_transitions)
+                           + go_to_impossible_transitions + game_controle_transitions)
 
         self.robot_state_machine = Machine(self, states=states, transitions=all_transitions, initial='stand_still')
 
@@ -136,8 +170,7 @@ class StateMachine():
         else:
             print('Aguardando o GameController para transicionar...')
             return False
-        #elif self.start_game == GAME_STATE_INITIAL:
-            #print("Estado Inicial do GameController, aguardando...\n--
+
     ########################################FUNÇÕES UPDATE CONDITION########################################
 
     def getting_up_condition_update(self, fall_state):
@@ -160,7 +193,17 @@ class StateMachine():
     def getting_up_condition(self): return self.getting_up_condition
     def impossible_condition(self): return False
 
-
+    def initial_condition(self): 
+        return self.game_controller_status == GAME_STATE_INITIAL #verifica se o game_controler está no estado inicial.
+    def ready_condition(self): 
+        return self.game_controller_status == GAME_STATE_READY #verifica se o game_controler está no estado "pronto"
+    def set_condition(self): 
+        return self.game_controller_status == GAME_STATE_SET #verifica se o game_controler está no estado "definido"
+    def play_condition(self): 
+        return self.game_controller_status == GAME_STATE_PLAYING #verifica se o game_controler está no estado "jogo"
+    def finished_condition(self): 
+        return self.game_controller_status == GAME_STATE_FINISHED #verifica se o game_controler está no estado "acabou"
+    
 def main(args=None):
     rclpy.init(args=args)
     state_machine = StateMachine()
@@ -169,5 +212,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == '_main_':
     main()
