31d7351b8821407ed04378c88c42a8fc0fcb5549
Paulo <paulo.barbosa.botelho@gmail.com>
2024-11-13 14:34:28 -0300

    fix atuadores.ino

--- src/movement/PID_codes/ESP32/esp_atuadores_espnow/esp_atuadores_espnow.ino
+++ src/movement/PID_codes/ESP32/esp_atuadores_espnow/esp_atuadores_espnow.ino
@@ -56,7 +56,8 @@ void timer_callback(rcl_timer_t* timer, int64_t last_call_time) {
   RCLC_UNUSED(last_call_time);
   if (timer != NULL) {
     //RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
- }}
+ }
+ }
 //======================================
 
 // ================ PID ================
